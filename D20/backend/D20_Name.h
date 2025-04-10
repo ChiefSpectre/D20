e47076ccbc1918cc9ac1714d5a78f98c986edd14
ChiefSpectre <chiefspectre@gmail.com>
2025-04-10 10:58:46 -0400

    updated name block
    Editable fields that takes from global vars

--- D20/backend/D20_Name.h
+++ D20/backend/D20_Name.h
@@ -5,15 +5,32 @@
 
 // Doc: When writing the container_size, if set to 0, it automatically adjusts.
     char charaName[35] = "";
+    char raceName[35] = "";
+    char className[15] = "";
 static inline void D20NameBlock()
 {
-
+    static bool editName = false;
 
     ImVec2 container_size(500, 100);  // Fixed width and height.
     ImGui::BeginChild("Container_1", container_size, true, ImGuiWindowFlags_None);
+    // Toggle Edit Name
+    if (ImGui::Button("Edit")) {
+        editName = !editName;
+    }
+    if (editName) {
+        ImGui::InputText("##charaName", charaName, sizeof(charaName));
+        ImGui::InputText("##raceName", raceName, sizeof(raceName));
+        ImGui::InputText("##className", className, sizeof(className));
 
-    ImGui::Text("Character Name");
-    ImGui::InputText("##charaName",charaName,sizeof(charaName));
+    }
+    else {
+        ImGui::Text(charaName);
+        ImGui::Text(raceName);
+        ImGui::Text(className);
+
+    }
+    
+    
     ImGui::Spacing();
     ImGui::EndChild();
 
